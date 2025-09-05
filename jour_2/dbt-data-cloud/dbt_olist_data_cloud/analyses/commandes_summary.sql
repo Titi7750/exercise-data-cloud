cdae0e0820915cea64b3ed77761047bb13418be9
Titi <tristanfio05@gmail.com>
2025-09-05 12:11:20 +0200

    [DEV]: Add SQL request & screen results for analyses

--- jour_2/dbt-data-cloud/dbt_olist_data_cloud/analyses/commandes_summary.sql
+++ jour_2/dbt-data-cloud/dbt_olist_data_cloud/analyses/commandes_summary.sql
@@ -5,3 +5,15 @@ select
     min(order_purchase_timestamp) as date_premiere_commande,
     max(order_purchase_timestamp) as date_derniere_commande
 from {{ ref('int_commandes_clients') }}
+
+WITH cmd_par_client AS (
+  SELECT
+    customer_id,
+    COUNT(DISTINCT order_id) AS nb_cmd
+  FROM {{ ref('int_commandes_clients') }}
+  GROUP BY 1
+)
+SELECT
+  COUNT(*) AS total_clients,
+  COUNTIF(nb_cmd >= 2) AS clients_recurrents
+FROM cmd_par_client;
